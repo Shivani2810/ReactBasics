297f8a6634be52b1a4ca8e01b359a3cab2c5fb93
Shivani2810 <108538382+Shivani2810@users.noreply.github.com>
2026-02-11 12:16:26 +0530

    Added controlled input using useState and implemented useEffect to track text changes

--- reactfoliotwo/src/App.js
+++ reactfoliotwo/src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
+// in whatever component the useeffect hook is writtem; it will run the code in useeffect after the component renders
+
+
 
 function App() {
+
+
+  // variation1 
+
+  // useEffect(()=>{
+  //   console.log("UI rendering done");
+  // });
+
+  // variation2- second parameter shows the dependency list
+
+  // useEffect(()=>{
+  //   console.log("UI rendering done");
+  // },[]);
+
+  // variation-3->on rendering the 
+
+ 
+
+
+
+  const[text,setText]=useState('a');
+  function changeHandler(event)
+  {
+    setText(event.target.value);
+     console.log(text);
+  }
+  useEffect(()=>{
+    console.log("Change seen");
+  },[text]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input type="text" onChange={changeHandler}></input> */}
+
+      {/* controlled input */}
+
+      <input 
+  type="text" 
+  value={text}
+  onChange={changeHandler}
+/>
+
     </div>
+
+
   );
 }
 
